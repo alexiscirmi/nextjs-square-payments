97f12249264d8c57fad1e217dae506b6bc12f91d
alexiscirmi <1320538+alexiscirmi@users.noreply.github.com>
2024-03-03 14:25:44 -0300

    Link to sandbox credit card numbers added

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -13,7 +13,14 @@ export default function Checkout({ params }) {
         <h1 className='pt-10 text-4xl'>{product.name}</h1>
         <h2 className='mt-10 text-xl'>Price: $ {product.price}</h2>
         <SquareProvider product={product} />
-        <div className='absolute flex justify-center bottom-20 w-full'>
+        <div className='absolute flex flex-col justify-center items-center gap-5 bottom-16 w-full'>
+          <a
+            href='https://developer.squareup.com/docs/devtools/sandbox/payments#web-and-mobile-client-testing'
+            target='_blank'
+            className='underline text-blue-900'
+          >
+            Sandbox credit card numbers
+          </a>
           <Link
             href='/'
             className='bg-blue-950 text-white border border-blue-500 rounded-md flex justify-center items-center w-20 h-8 hover:scale-105 transition-all'
