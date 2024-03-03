1924bfa8405f5d79a50c6a9ba612697cb164767e
alexiscirmi <1320538+alexiscirmi@users.noreply.github.com>
2024-03-03 05:03:33 -0300

    UI tweak

--- app/components/card-list/card-list.jsx
+++ app/components/card-list/card-list.jsx
@@ -5,7 +5,7 @@ export const CardList = () => {
   return (
     <section
       id='card-list'
-      className='mt-20 gap-10 flex flex-wrap justify-center items-center text-center'
+      className='mt-20 mx-5 gap-10 flex flex-wrap justify-center items-center text-center'
     >
       {products.list.map((product) => (
         <Card
